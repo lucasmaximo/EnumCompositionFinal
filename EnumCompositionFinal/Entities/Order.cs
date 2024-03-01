30db490b79f3a0c922ac90d7904d0ae8f0fd965a
Lucas Máximo <lucasmaximo@outlook.com>
2024-03-01 11:01:43 -0300

    Adicionar arquivos de projeto.

---
+++ EnumCompositionFinal/Entities/Order.cs
@@ -0,0 +1,61 @@
+﻿using EnumCompositionFinal.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumCompositionFinal.Entities
+{
+    internal class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus  Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
+        public Order()
+        { 
+        }
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            OrderItems.Add(item);
+        }
+        public void RemoveItem(OrderItem item)
+        {
+            OrderItems.Remove(item);
+        }
+        public double Total()
+        {
+            double total = 0;
+            foreach (OrderItem item in OrderItems)
+            {
+                total += item.SubTotal();
+            }
+            return total;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Order moment: " + Moment.ToString("dd/MM/yyyy hh:mm:ss"));
+            sb.AppendLine("Order Status: " + Status);
+            sb.AppendLine("Client: " + Client);
+            sb.AppendLine("Order itens: ");
+            foreach (OrderItem item in OrderItems)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            sb.AppendLine("Total price: $" + Total().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+    }
+}
---
+++ EnumCompositionFinal/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EnumCompositionFinal.Entities;
+using EnumCompositionFinal.Entities.Enums;
+using System.Globalization;
+
+Console.WriteLine("Enter client data: ");
+Console.Write("Name: ");
+string name = Console.ReadLine();
+Console.Write("Email: ");
+string email = Console.ReadLine();
+Console.Write("Birth date (DD/MM/YYYY): ");
+DateTime birthDate = DateTime.Parse(Console.ReadLine());
+Console.WriteLine("Enter order data: ");
+Console.Write("Status: ");
+OrderStatus orderStatus = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+Client client = new Client(name, email, birthDate);
+Order order = new Order(DateTime.Now, orderStatus, client);
+
+Console.Write("How many items to this order? ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine($"Enter #{i} item data:");
+    Console.Write("Product Name: ");
+    string productName = Console.ReadLine();
+    Console.Write("Product Price: ");
+    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    Product product = new Product(productName, price);
+
+    Console.Write("Quantity: ");
+    int quantity = int.Parse(Console.ReadLine());
+
+    OrderItem orderItem = new OrderItem(quantity, price, product);
+
+    order.AddItem(orderItem);
+}
+
+Console.WriteLine();
+Console.WriteLine("ORDER SUMMARY");
+Console.WriteLine(order);
